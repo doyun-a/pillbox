a67d0a4c7d1581631820018d837adffcc416f631
doyun <youn6432@naver.com>
2024-12-24 17:12:56 +0900

    first setting

---
+++ src/main/java/com/example/pillbox/PillboxApplication.java
@@ -0,0 +1,18 @@
+package com.example.pillbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PillboxApplication.class, args);
+
+		System.out.print("hello");
+
+
+
+	}
+
+}
---
+++ src/test/java/com/example/pillbox/PillboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pillbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
